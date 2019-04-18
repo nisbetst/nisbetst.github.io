53e860168f201f944d1cf649e09783b372a88b4f
nisbetst <46688025+nisbetst@users.noreply.github.com>
2019-04-18 10:42:03 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Broadcast Bar and cookies on home page
 var broadcast = document.querySelector('.broadcast');
 var closeBroadcast = document.querySelector('.hide-broadcast-target');
 var cookie = readCookie('hide_broadcast_bar');
@@ -39,6 +40,7 @@ function eraseCookie(name) {
   createCookie(name, "", -1);
 }
 
+// Hamburger Menu
 function hamburger() {
   var x = document.getElementById("navbar");
   if (x.className === "mynav") {
@@ -46,5 +48,28 @@ function hamburger() {
   }
   else {
     x.className = "mynav";
+  }
+}
+
+// Modal
+
+function show_modal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function hide_modal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var i = 0;
+var txt = "My name is Stephen Nisbet and I am a UX researher, designer, and life enthusiast. These are the projects I've worked on and this is their story.";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("home-txt").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
 }
